d01ec098d66a9d535880c2869863738a98e50593
TeodorH <teodor.hornfeldt07@gmail.com>
2025-11-27 13:46:22 +0100

    Ändrat header, ändrat utsendet på matchhistorik, där matcher som ai programmet förutsätt rätt är gröna och dem som inte träffade blir röda. Har dessutom lagt till typ 1 för ol så dem får nummer.

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((res) => res.json())
     .then((data) => {
       const container = document.getElementById("match-historik");
-      const ul = document.createElement("ul");
+      const ol = document.createElement("ol");
+      ol.type = "1";
       data.bets.forEach((b) => {
         const li = document.createElement("li");
         li.textContent = b.match;
-        ul.appendChild(li);
+        if (b.winner === b["real-winner"]) {
+          li.classList.add("match-correct");
+        } else {
+          li.classList.add("match-incorrect");
+        }
+        ol.appendChild(li);
       });
-      container.appendChild(ul);
+      container.appendChild(ol);
     });
 });
